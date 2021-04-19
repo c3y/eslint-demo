25ea41e2daafaab2638c53b5cbf5d650811e639f
c3y <alpceyhun@hotmail.com>
2021-04-20 01:44:06 +0300

    final commit for eslint demo..

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 let didShowMessage = false;
 
 function showMessage() {
   const message = "I like logs";
 
   for (let i = 0; i < 5; i++) {
-    console.log(message);
+    console.log(message)
   }
 }
 
